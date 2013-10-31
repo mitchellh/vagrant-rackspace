195b9582ae66343999fb11eebcbb18d273a965c4
Max Lincoln <max@devopsy.com>
2013-10-31 17:38:00 -0300

    add tty workaround middleware

---
+++ lib/vagrant-rackspace/action/tty_workaround.rb
@@ -0,0 +1,33 @@
+require "log4r"
+require 'rbconfig'
+require "vagrant/util/subprocess"
+
+module VagrantPlugins
+  module Rackspace
+    module Action
+      # This middleware fixes the sudoers file so it allows sudo without a tty.
+      class TTYWorkaround
+        def initialize(app, env)
+          @app    = app
+          @logger = Log4r::Logger.new("vagrant_rackspace::action::sync_folders")
+          @host_os = RbConfig::CONFIG['host_os']
+        end
+
+        def call(env)
+          @app.call(env)
+          ssh_info = env[:machine].ssh_info
+
+          env[:ui].info(I18n.t("vagrant_rackspace.requiretty_workaround"))
+          fog_ssh = Fog::SSH.new(ssh_info[:host], ssh_info[:username], {:keys => [ssh_info[:private_key_path]]})
+          fog_scp = Fog::SCP.new(ssh_info[:host], ssh_info[:username], {:keys => [ssh_info[:private_key_path]]})
+
+          workaround_script = VagrantPlugins::Rackspace.source_root.join("resources/require_tty_workaround.sh")
+
+          fog_scp.upload(workaround_script.to_s, '/tmp/require_tty_workaround.sh')
+          results = fog_ssh.run("sudo bash /tmp/require_tty_workaround.sh")
+          env[:ui].info(results.map(&:stdout))
+        end
+      end
+    end
+  end
+end
