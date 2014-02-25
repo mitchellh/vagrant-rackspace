8775b4e8acf74d15f5c9e2b29a522ca28513bb54
Max Lincoln <max@devopsy.com>
2014-02-25 16:48:14 -0500

    Pull out spec_helper from existing test

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+if ENV['COVERAGE'] != 'false'
+  require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.start
+end
+
+require 'fog'
