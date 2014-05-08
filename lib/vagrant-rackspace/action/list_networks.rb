8b4f486edbbf5a166dca4b4f105a8ecb8d5a3b03
Christian Berendt <berendt@b1-systems.de>
2014-05-08 09:57:08 +0200

    added column Network ID to the network list
    ```
    ==> devstack: Network Name                         Network CIDR             Network ID
    ==> devstack: private                                                       11111111-1111-1111-1111-111111111111
    ==> devstack: public                                                        00000000-0000-0000-0000-000000000000
    ==> devstack: vagrant001                           192.168.3.0/24           c7da5d07-ac80-49ca-8575-8822c71bc758
    ==> devstack: vagrant002                           192.168.4.0/24           f45ba323-dd21-4999-a5d7-c98a3edfb7a3
    ==> devstack: vagrant003                           192.168.5.0/24           e49a86df-6c98-42d1-a66b-11c80e1252e1
    ```

--- lib/vagrant-rackspace/action/list_networks.rb
+++ lib/vagrant-rackspace/action/list_networks.rb
@@ -8,9 +8,9 @@ module VagrantPlugins
 
         def call(env)
           compute_service = env[:rackspace_compute]
-          env[:ui].info ('%-36s %s' % ['Network Name', 'Network CIDR'])
+          env[:ui].info ('%-36s %-24s %s' % ['Network Name', 'Network CIDR', 'Network ID'])
           compute_service.networks.sort_by(&:label).each do |network|
-            env[:ui].info ('%-36s %s' % [network.label, network.cidr])
+            env[:ui].info ('%-36s %-24s %s' % [network.label, network.cidr, network.id])
           end
           @app.call(env)
         end
