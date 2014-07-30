e20fdea14cbe2b2d45d36e5fa1d7e073c83f6165
Peter Souter <p.souter@kainos.com>
2014-07-30 16:52:07 +0100

    Adding log output for provisioner run

--- lib/vagrant-rackspace/action/tty_workaround.rb
+++ lib/vagrant-rackspace/action/tty_workaround.rb
@@ -9,13 +9,15 @@ module VagrantPlugins
       class TTYWorkaround
         def initialize(app, env)
           @app    = app
-          @logger = Log4r::Logger.new("vagrant_rackspace::action::sync_folders")
+          @logger = Log4r::Logger.new("vagrant_rackspace::action::requiretty_workaround_start")
           @host_os = RbConfig::CONFIG['host_os']
         end
 
         def call(env)
           @app.call(env)
+          env[:ui].info(I18n.t("vagrant_rackspace.requiretty_workaround_start"))
           env[:machine].communicate.sudo 'sed -i\'.bk\' -e \'s/^\(Defaults\s\+requiretty\)/# \1/\' /etc/sudoers'
+          env[:ui].info(I18n.t("vagrant_rackspace.requiretty_workaround_end"))
         end
       end
     end
